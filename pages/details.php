f3fb403243061c445aba8669350ae81bff3c27ca
Badrien <adrienboppe@gmail.com>
2017-01-10 14:23:30 +0100

    modal Js
    le modal dépend d'un fichier javascript

--- pages/details.php
+++ pages/details.php
@@ -1,6 +1,3 @@
 <?php $offer = $db->prepare('SELECT * FROM t_offers WHERE id = ?', [$_GET['id']], 'App\Offer', true); ?>
-
-
 <h1><?php echo $offer->getTitle(); ?></h1>
 <p><?php echo $offer->getContent(); ?></p>
-<button type="button" data-toggle="modal" data-target="#modalForm" name="buttonForm">Postuler</button>
