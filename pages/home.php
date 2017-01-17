ecaa5c0b2ceed90347b20e98814a735c4ca32507
MSI\micha <michael.ringelstein@laposte.net>
2017-01-17 11:11:32 +0100

    mis a niveau checkboxs

--- pages/home.php
+++ pages/home.php
@@ -23,52 +23,60 @@ $_SESSION['offers_selected'] = 0;
             <div class="col-md-12 resultats"> RESULTATS</div>
 
 
-  <?php   //$paire = ($i%2 == 0) ? true : false;
-  foreach ($db->query('SELECT * FROM t_offers', 'App\Offer') as $offer)
-  { ?>
-      <a  class="modalBtn" href="#" role="button" data-toggle="modal" data-target="#modal">
-        <div class="col-md-12">
-          <span>
-            <div class="linkadd">
-              <h3 class="annonces title-offers"> <?php echo $offer->getTitle(); ?></h3>
-              <div class="row">
-                <div class="col-md-offset-1 col-md-2">
-                  <i class="glyphicon glyphicon-calendar icon-offer"></i>
-                  <span class="annonces"><?php echo $offer->getIntern_duration(); ?></span>
-                </div>
 
-                <div class="col-md-offset-1 col-md-2">
-                  <i class="glyphicon glyphicon-map-marker icon-offer"></i>
-                  <span class="annonces">  <?php echo $offer->getRegion(); ?></span>
-                </div>
+      <?php foreach ($db->query('SELECT * FROM t_offers', 'App\Offer') as $offer)
+                  { ?>
 
-                <div class="col-md-offset-1 col-md-2">
-                  <i class="glyphicon glyphicon glyphicon-education icon-offer"></i>
-                  <sapn class="annonces"> <?php echo $offer->getintern_level(); ?></span>
-                </div>
-              </div>
-            </div>
-          </span>
-        </div>
-      </a>
-      <!--Modal-->
-      <div class="modal fade modalForm" role="dialog"  tabindex="-1" aria-labelledby="modalFormLabel">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-               <button type="button" class="close" id="modalClose" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-              <h4>Postuler pour une offre :</h4>
-            </div>
-              <div class="modal-body">
-                <h1><?php echo $offer->getTitle(); ?></h1>
-                <p><?php echo $offer->getContent(); ?></p>
-              </div>
-              <div class="modal-footer">
-              </div>
-          </div>
-        </div>
-      </div>
-      <?php } ?>
+                    <a class="" href="#" id = "modalBtn" role="button" data-toggle="modal" data-target=<?php echo "#modal".$offer->getId();?>>
+                      <div class="col-md-11">
+                        <span>
+                          <div class="linkadd">
+                            <h3 class="annonces title-offers"> <?php echo $offer->getTitle(); ?></h3>
+                            <div class="row">
+                              <div class="col-md-offset-1 col-md-2">
+                                <i class="glyphicon glyphicon-calendar icon-offer"></i>
+                                <span class="annonces"><?php echo $offer->getIntern_duration(); ?></span>
+                              </div>
+
+                              <div class="col-md-offset-1 col-md-2">
+                                <i class="glyphicon glyphicon-map-marker icon-offer"></i>
+                                <span class="annonces">  <?php echo $offer->getRegion(); ?></span>
+                              </div>
+
+                              <div class="col-md-offset-1 col-md-2">
+                                <i class="glyphicon glyphicon glyphicon-education icon-offer"></i>
+                                <sapn class="annonces"> <?php echo $offer->getintern_level(); ?></span>
+                              </div>
+                            </div>
+                          </div>
+                        </span>
+                      </div>
+                    </a>
+
+                    <div class="col-md-1">
+                 <input type="checkbox" name=<?php echo "checkbox".$offer->getId();?> id="checkbox">
+                   </div>
+
+                  <!--Modal-->
+                  <div class="modal fade modalForm" role="dialog" id =<?php echo "modal". $offer->getId(); ?> tabindex="-1" aria-labelledby="modalFormLabel">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                           <button type="button" class="close" id="modalClose" aria-label="Close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+                          <h4>Postuler pour une offre :</h4>
+                        </div>
+                          <div class="modal-body">
+                            <h1><?php echo $offer->getTitle(); ?></h1>
+                            <p><?php echo $offer->getContent(); ?></p>
+                          </div>
+                          <div class="modal-footer">
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+            <?php } ?>
+
+
 
 
           </div>
@@ -89,4 +97,4 @@ $_SESSION['offers_selected'] = 0;
   </div>
 </div> <!-- /.container -->
 
-<script src="../public/js/modal.js"></script>
+<script src="../public/js/checkbox.js"></script>
