cccbd52eb077ca84a5f1bc3ace3035d6dde1abb7
Amandine01 <amandine.ducruet@free.fr>
2017-01-20 16:02:54 +0100

    C'est reparti
    Fichier sql ou est tu

--- src/t_candidatures/content.sql
+++
@@ -1,2 +0,0 @@
-insert into t_offers values
-(1,3,1);
--- src/t_candidatures/structure.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists t_candidatures;
-create table t_candidatures (
-id integer not null primary key auto_increment,
-offer_id integer,
-candidate_id integer not null
-) engine=innodb character set utf8 collate utf8_unicode_ci;
